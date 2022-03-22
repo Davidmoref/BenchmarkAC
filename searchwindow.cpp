c9166d6bce8992f5bc5afb04ba7e32e83142c219
pablopane <72921696+ppaner00@users.noreply.github.com>
2022-03-22 16:16:07 +0100

    Intento de avance con algoritmo

--- searchwindow.cpp
+++ searchwindow.cpp
@@ -1,5 +1,6 @@
 #include "searchwindow.h"
 #include "ui_searchwindow.h"
+#include <iostream>
 
 SearchWindow::SearchWindow(QWidget *parent) :
     QDialog(parent),
@@ -19,5 +20,44 @@ void SearchWindow::on_volver_clicked()
     this->hide();
     QWidget *parent = this->parentWidget();
     parent->show();
+}
+
+void SearchWindow::algoritmo(){
+
+    int n, i, A[30], num, primero, ultimo, medio;
+       std::cout<<"Ingrese un arreglo ordenado: ";
+       std::cout<<"Cuantos elementos te gustaria ingresar?: ";
+       std::cin>>n;
+
+       for (i=0; i>A[i];i++)
+       {
+           std::cout<<"Ingrese el numero que desea buscar: ";
+           std::cin>>num;
+       }
+       primero=0;
+       ultimo=n-1;
+       medio=(primero+ultimo)/2;
+       while (primero<=ultimo)
+       {
+           if (A[medio]< num)
+           {
+               primero=medio+1;
+           } else if (A[medio]== num)
+           {
+               std::cout<<" Se encontro la posición ";
+               std::cout<<medio+1;
+               break;
+           }
+           else {
+               ultimo = medio - 1;
+           }
+
+           medio = (primero+ultimo)/2;
+       }
+       if (primero>ultimo)
+       {
+           std::cout<<num<<" no se encontro";
+       }
+
 }
 
